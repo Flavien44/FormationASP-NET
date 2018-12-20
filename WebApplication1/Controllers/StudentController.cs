82ac10880e3db341039f2502e9a067b0b2146881
Flavien <flavienangelo@hotmail.com>
2018-12-20 15:22:51 +0100

    formation

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            var student = new Student
+            {
+                Name = "Contoso Jon",
+                Class = 1,
+                Email = "12345@gmail.com",
+                HasPaid = true
+            };
+            return View(student);
+        }
+
+        public ActionResult Edit()
+        {
+            var s = new Student
+            {
+                Name = "Contoso Jon",
+                Class = 1,
+                Email = "12345@gmail.com",
+                HasPaid = true
+            };
+            return View(s);
+        }
+
+        public ViewResult Detail()
+        {
+            return View(new DetailViewModel
+            {
+                Student = new Student
+                {
+                    Name = "Contoso Jon",
+                    Class = 1,
+                    Email = "12345@gmail.com",
+                    HasPaid = true
+                },
+                School = "INFO"
+            });
+        }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models
+{
+
+    public class Student
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int StudentID { get; set; }
+
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public int Class { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",
+            ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime JoinDate { get; set; }
+
+        [Display(Name = "Fees paid")]
+        public bool HasPaid { get; set; }
+    }
+}
